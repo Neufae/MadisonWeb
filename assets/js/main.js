230aed7eb863a544159a4b68e96e056287b5500f
Natalie Langevoort <neufae@yahoo.com>
2026-04-12 18:15:24 -0400

    Making final changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -98,13 +98,15 @@ window.addEventListener('scroll', function () {
       window.scrollY > 100 ? scrollTop.classList.add('active') : scrollTop.classList.remove('active');
     }
   }
-  scrollTop.addEventListener('click', (e) => {
-    e.preventDefault();
-    window.scrollTo({
-      top: 0,
-      behavior: 'smooth'
+  if (scrollTop) {
+    scrollTop.addEventListener('click', (e) => {
+      e.preventDefault();
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+      });
     });
-  });
+  }
 
   window.addEventListener('load', toggleScrollTop);
   document.addEventListener('scroll', toggleScrollTop);
